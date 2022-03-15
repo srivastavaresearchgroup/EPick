f3eb390f69a0bffb09f8092456e5bfb86ff4c223
Wren <49800396+Wren-Meta@users.noreply.github.com>
2022-03-15 18:17:52 +0100

    Update epick_test.py

--- Test/Epick/epick_test.py
+++ Test/Epick/epick_test.py
@@ -23,8 +23,8 @@ from matplotlib.ticker import FuncFormatter
 import math
 
 # initial setting
-FLAGS = None
-def result_static_fun3(predict_images, t_labels):
+args = None
+def result(predict_images, t_labels):
   TE = FE = 0     ## TE: true earthquake and FE: false earthquake
   TN = FN = 0     ## TN: true pure noise and FN: false noise
   Final_true = []
@@ -37,7 +37,7 @@ def result_static_fun3(predict_images, t_labels):
   d_ss = []
   batch_size = predict_images.shape[0]
   for i in range(batch_size):
-    image_array = predict_images[i, :, :]  ## [image_size, 3]
+    image_array = predict_images[i, :, :]
     p1 = np.argmax(image_array[:, 1], axis = 0)
     for j in range(image_array.shape[0]):
       if j!= p1:
@@ -103,20 +103,20 @@ def result_static_fun3(predict_images, t_labels):
 def test():
     tf.compat.v1.set_random_seed(24)
     cfg = config.Config()
-    cfg.batch_size = FLAGS.batch_size
+    cfg.batch_size = args.batch_size
     cfg.add = 1
-    cfg.n_clusters = FLAGS.num_classes
+    cfg.n_clusters = args.num_classes
     cfg.n_clusters += 1
     cfg.n_epochs = 1
 
-    pos_pipeline = dp.DataPipeline(FLAGS.tfrecords_dir, cfg, False)
-    images = pos_pipeline.samples 
+    pos_pipeline = dp.DataPipeline(args.tfrecords_dir, cfg, False)
+    waveform = pos_pipeline.samples 
     labels = pos_pipeline.labels   
-    logits = model(images, FLAGS.num_classes, True) 
+    logits = model(waveform, args.num_classes, True) 
     
     accuracy = accuracy(logits, labels)
-    loss = loss(logits, labels, FLAGS.weight_decay_rate)
-    predicted_images = predict2(logits, FLAGS.batch_size, FLAGS.image_size) 
+    loss = loss(logits, labels, args.weight_decay_rate)
+    prediction = predict2(logits, args.batch_size, args.waveform_length) 
 
     ## session initialization
     init_op = tf.compat.v1.group(tf.compat.v1.global_variables_initializer(), 
@@ -125,12 +125,12 @@ def test():
     sess.run(init_op)
     saver = tf.compat.v1.train.Saver() 
     
-    ## restore modeltf.io.gfile
-    if not tf.io.gfile.exists(FLAGS.checkpoint_path + '.meta'):
+    ## loading savee model
+    if not tf.io.gfile.exists(args.checkpoint_path + '.meta'):
         raise ValueError("Can't find checkpoint file")
     else:
         print('[INFO   ]\t Found checkpoint file, restoring model.') 
-        saver.restore(sess, FLAGS.checkpoint_path)  # reload the trained model
+        saver.restore(sess, args.checkpoint_path)  # reload the trained model
 
     coord = tf.compat.v1.train.Coordinator() 
     threads = tf.compat.v1.train.start_queue_runners(sess = sess, coord = coord) 
@@ -153,7 +153,7 @@ def test():
         while not coord.should_stop():
             loss_value, predicted_images_value, images_value,  ss_labels= sess.run([loss, predicted_images, images, labels])
     
-            TE, FE, TN, FN, Final_pre, Final_true, Final_p, Final_s, false_p, false_s = result_static_fun3(predicted_images_value, ss_labels)
+            TE, FE, TN, FN, Final_pre, Final_true, Final_p, Final_s, false_p, false_s = result(predicted_images_value, ss_labels)
             total_TE += TE
             total_FE += FE
             total_TN += TN
@@ -170,25 +170,23 @@ def test():
             step+= 1
 
     except tf.errors.OutOfRangeError:
-        print('[INFO    ]\t Done evaluating in %d steps.' % step)
+        print('Finishing in %d steps.' % step)
     finally:
         """When done, ask the threads to stop."""
         coord.request_stop()
 
-    doc = open('./test.txt', 'w')
-
-    ''' metric of earthquake '''
-    E_accuracy = (total_TE + total_TN)/(step* FLAGS.batch_size)
+    ''' Metrics of earthquake detection '''
+    E_accuracy = (total_TE + total_TN)/(step*args.batch_size)
     E_precision = total_TE/(total_TE + total_FE)
     E_recall =  total_TE/(total_TE + total_FN)
     E_f1 = 2 * E_precision * E_recall/(E_precision + E_recall)
-    print("earthquake_detection_accuracy: ", E_accuracy, file = doc)
-    print("earthquake_detection_precision: ", E_precision, file = doc)
-    print("earthquake_detection_recall: ",  E_recall, file = doc)
-    print("earthquake_detection_f1: ", E_f1, file = doc)
+    print("earthquake_detection_accuracy: ", E_accuracy)
+    print("earthquake_detection_precision: ", E_precisionc)
+    print("earthquake_detection_recall: ",  E_recall)
+    print("earthquake_detection_f1: ", E_f1)
 
 
-    ''' plot confusion matrix of earthquake and non-earthqule '''
+    ''' Confusion matrix for earthquake and non-earthqule '''
     cm1= confusion_matrix([0, 0, 0, 0], [1, 1, 1, 1])  ## initialize
     cm1[0][0] = total_TN
     cm1[0][1] = total_FE
@@ -209,9 +207,8 @@ def test():
     plt.imshow(cm1, interpolation='nearest', cmap=plt.cm.GnBu)
     plt.title('Confusion Matrix')
     plt.colorbar()
-    plt.savefig('./earthquake_detection_confusion_matrix.jpg', dpi =600)
-    # plt.show()
-    plt.close()
+    plt.savefig('./earthquake_detection_confusion_matrix.jpg')
+    plt.show()
 
     cm2 = confusion_matrix(F_t, F_p) 
     multi_label2 = ['Non-arrival', 'P-arrival', 'S-arrival' ]
@@ -229,23 +226,21 @@ def test():
     plt.imshow(cm2, interpolation='nearest', cmap=plt.cm.GnBu)
     plt.title('Confusion Matrix')
     plt.colorbar()
-    plt.savefig('./phase_detection_confusion_matrix.jpg', dpi =350)
-    plt.close()
+    plt.savefig('./phase_detection_confusion_matrix.jpg')
+    plt.show()
 
     Final_accuracy = (cm2[0][0]+cm2[1][1]+cm2[2][2])/(cm1[1][1]*6000)
     Final_precision = precision_score(F_t, F_p, average = 'weighted')
     Final_recall = recall_score(F_t, F_p, average = 'weighted')
     Final_f1 = f1_score(F_t, F_p, average = 'weighted')
-    print("Final_detection_accuracy: ",Final_accuracy, file = doc)
-    print("Final_detection_precision: ", Final_precision, file = doc)
-    print("Final_detection_recall: ",  Final_recall, file = doc)
-    print("Final_detection_f1: ", Final_f1, file = doc)
+    print("Final_detection_accuracy: ",Final_accuracyc)
+    print("Final_detection_precision: ", Final_precision)
+    print("Final_detection_recall: ",  Final_recall)
+    print("Final_detection_f1: ", Final_f1)
     
-    
-    t = classification_report(F_t, F_p, target_names=['Neither', 'P phase', 'S phase'], digits=5)
-    print(t, file = doc)
-    
-    ''''statistic'''
+   
+    print('classification report: ', classification_report(F_t, F_p, target_names=['Neither', 'P phase', 'S phase'], digits=5))
+   
     p_error = [int(Final_p_error[i])/100.0 for i in range(len(Final_p_error))] ## converting smaples to time (s)
     p_error_mean = sum(p_error)/len(p_error)
     p_error_std = statistics.stdev(p_error)
@@ -255,16 +250,9 @@ def test():
     s_error_std = statistics.stdev(s_error)
 
     print("P pick mean = ", p_error, file=doc)
-    print("P pick standard deviation = ", p_error_std, file = doc)
+    print("P pick standard deviation = ", p_error_std)
     print("S pick mean = ", s_error , file = doc)
-    print("S pick standard deviation = ", s_error_std ,file = doc)
-
-    ## error saving
-    df1 = pd.DataFrame(Final_p_error)
-    df1.to_csv('./P_pick.csv', index=False)
-    df2 = pd.DataFrame(Final_s_error)
-    df2.to_csv('./S_pick.csv', index=False)
-
+    print("S pick standard deviation = ", s_error_std)
 
     ''' plot P-arrival time error histogram '''
     plt.figure()
@@ -291,10 +279,10 @@ def test():
     sess.close()
 
 def main(_):
-    if FLAGS.output_dir is not None:
-        if not os.path.exists(FLAGS.output_dir):
-            print('[INFO   ]\t Output directory does not exist, creating directory: ' + os.path.abspath(FLAGS.output_dir))
-            os.makedirs(FLAGS.output_dir)
+    if args.output_dir is not None:
+        if not os.path.exists(args.output_dir):
+            print('[INFO   ]\t Output directory does not exist, creating directory: ' + os.path.abspath(args.output_dir))
+            os.makedirs(args.output_dir)
     test()
 
 if __name__ == '__main__':
@@ -304,11 +292,11 @@ if __name__ == '__main__':
     parser.add_argument('--checkpoint_path',  help ='Path of checkpoint to restore',
                        default = './model.ckpt')
     parser.add_argument('--num_classes',      help = 'Number of segmentation labels', type = int, default = 3)
-    parser.add_argument('--image_size',       help = 'Target image size (resize)', type = int, default = 6000)
+    parser.add_argument('--waveform_length',       help = 'Target image size (resize)', type = int, default = 6000)
     parser.add_argument('--batch_size',       help = 'Batch size', type = int, default = 4)
     parser.add_argument('--output_dir',
                         help = 'Output directory for the prediction files. If this is not set then predictions will not be saved',
                         default = './output/')
     parser.add_argument('--weight_decay_rate', help = 'Weight decay rate', type = float, default = 0.0005)                    
-    FLAGS, unparsed = parser.parse_known_args()
+    args, unparsed = parser.parse_known_args()
     tf.compat.v1.app.run()
